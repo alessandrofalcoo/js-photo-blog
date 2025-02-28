9ec2f32b3d88f33e5ce1350af7d605ab751a1735
alessandrofalcoo <alefalco.1003@gmail.com>
2025-02-28 14:59:36 +0100

    fix the loop with a forEach instead of a for loop

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,38 @@
-const cardEl = document.querySelector('.my-card');
-const imgEl = document.querySelector('.my-img');
 
+// Select the row
+const rowEl = document.querySelector('.row')
+
+
+// Use the fetch for the API 
 fetch('https://lanciweb.github.io/demo/api/pictures/')
     .then(response => response.json())
     .then(data => {
         console.log(data);
+
+        // Create a forEach loop to generate images from the API
+        data.forEach(element => {
+            console.log(element.url);
+            rowEl.innerHTML += ` <div class="col pb-3">
+                    <div class="card my-card">
+                        <div class="card-body">
+                            <div class="cork-container">
+                                <img class="cork" src="./assets/img/pin.svg" alt="">
+
+                            </div>
+                            <h3 class="card-title card-header"><img class="my-img" src="${element.url}" alt=""></h3>
+                            <small>${element.date}</small>
+                            <h4 class="card-text titles">${element.title}</h4>
+                        </div>
+                    </div>
+                </div>`
+        });
     })
 
     .catch(error => {
         console.error(error);
 
     })
-for (let i = 0; i < 5; i++) {
-    imgEl.innerHTML += `<img class="my-img" src="https://marcolanci.it/boolean/assets/pictures/.1/${imgEl}" alt="">`
-}
+
+
+console.log(cardHeaderEl.innerHTML);
+

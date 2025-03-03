c5a1f430810964cd123ee5ed06b59d3373820813
alessandrofalcoo <alefalco.1003@gmail.com>
2025-03-03 15:03:30 +0100

    add id to the hidden div and the button

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,8 @@
 
 // Select the row
 const rowEl = document.querySelector('.row')
-const hiddenEl = document.querySelector('.hidden-div')
-const buttonEl = document.querySelector('.my-button')
-
+const hiddenEl = document.getElementById('hidden-div')
+const buttonEl = document.getElementById('my-button')
 // Use the fetch for the API 
 fetch('https://lanciweb.github.io/demo/api/pictures/')
     .then(response => response.json())
@@ -14,27 +13,35 @@ fetch('https://lanciweb.github.io/demo/api/pictures/')
             console.log(element);
             // Use the row for writing inside in HTML code with template literal with element variable
             rowEl.innerHTML += ` <div class="col pb-3">
-                    <div class="card my-card">
-                        <div class="card-body">
-                            <div class="cork-container">
-                                <img class="cork" src="./assets/img/pin.svg" alt="">
-                            </div>
-                            <h3 class="card-title card-header"><img class="my-img" src="${element.url}" alt=""></h3>
-                            <small>${element.date}</small>
-                            <h4 class="card-text titles">${element.title}</h4>
-                        </div>
-                    </div>
-                </div>`
+        <div class="card my-card">
+        <div class="card-body">
+        <div class="cork-container">
+        <img class="cork" src="./assets/img/pin.svg" alt="">
+        </div>
+        <h3 class="card-title card-header"><img class="my-img" src="${element.url}" alt=""></h3>
+        <small>${element.date}</small>
+        <h4 class="card-text titles">${element.title}</h4>
+        </div>
+        </div>
+        </div>`
         })
         const imgEl = document.querySelectorAll('.my-img')
         console.log(imgEl);
         imgEl.forEach(element => {
             console.log(element);
+            const thisEl = element.src
+            console.log(thisEl);
             element.addEventListener('click', function () {
                 hiddenEl.classList.remove('d-none');
-
+                hiddenEl.innerHTML += `        
+            <img class="hidden-img" src="${thisEl}" alt="">`
             })
         })
+        const buttonEl = document.getElementById('my-button').addEventListener('click', () => {
+            console.log(buttonEl);
+            
+                hiddenEl.classList.add('d-none')
+            })
     })
 
 /* .catch(error => {
@@ -42,7 +49,4 @@ fetch('https://lanciweb.github.io/demo/api/pictures/')
     
     }) */
 
-buttonEl.addEventListener('click', function () {
-    hiddenEl.classList.add('d-none')
-})
 
